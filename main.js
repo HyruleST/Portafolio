684441b12f85f9cc130518673c9d9354bea62205
HyruleST <majoraraps366@gmail.com>
2022-11-27 21:50:30 -0600

    third

---
+++ main.js
@@ -0,0 +1,7 @@
+let anto = document.querySelector('#anto');
+let conec = document.querySelector('#conec');
+let net = document.querySelector('#net');
+
+anto.addEventListener('click', event => window.location.href = "./html/antologia.html");
+conec.addEventListener('click', event => window.location.href = "./html/conectividad.html");
+net.addEventListener('click', event => window.location.href = "./html/network.html");
